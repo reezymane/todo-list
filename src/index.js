a55bd598406762bd7ca61388ab459df44e2a077a
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-22 16:09:10 -0500

    Add functions to show and hide forms. Add funcionality to project cancel button

--- src/index.js
+++ src/index.js
@@ -2,11 +2,29 @@ import './style.css';
 import {todo, project} from './factories';
 
 (() => {
+    // Display and hide forms
+    function openPForm() {
+        document.getElementById('newProject').style.display = 'block';
+    };
+
+    function closePForm() {
+        document.getElementById('newProject').style.display = 'none';
+        document.getElementById('newProject').reset();
+    };
+
+    function openTDForm() {
+        document.getElementById('todo').style.display = 'block';
+    };
+
+    function closeTDForm() {
+        document.getElementById('todo').style.display = 'none';
+        document.getElementById('todo').reset();
+    };
+
     // Makes new project form appear when new project button is clicked
     const newProject = document.getElementsByClassName('newProject');
     newProject.item(0).addEventListener('click', (button) => {
-        const projectForm = document.getElementById('newProject');
-        projectForm.style.display = 'block';
+        openPForm();
     });
 
     // Creates a General project and an array to hold all projects
@@ -17,6 +35,13 @@ import {todo, project} from './factories';
     const projectSubmit = document.getElementById('projectSubmit');
     projectSubmit.addEventListener('click', () => {
         projects.push(project(document.getElementById('projectName').value));
-        console.log(projects);
+
+        closePForm();
+    });
+
+    // Cancel button closes project form
+    const projectCancel = document.getElementById('projectCancel');
+    projectCancel.addEventListener('click', () => {
+        closePForm();
     });
 })();
