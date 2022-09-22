3cb45479f6d58978796cf8dd0c43a9c3ddf72f69
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-22 14:41:55 -0500

    Make new project form appear when new project button is clicked

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './style.css';
 import {todo, project} from './factories';
+
+(() => {
+    // Makes new project form appear when new project button is clicked
+    const newProject = document.getElementsByClassName('newProject');
+    newProject.item(0).addEventListener('click', (button) => {
+        const projectForm = document.getElementById('newProject');
+        projectForm.style.display = 'block';
+    });
+})();
