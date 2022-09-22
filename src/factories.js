15b5c0138ad9ec0b555f4efa2bd7eedb9d760755
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-22 14:22:30 -0500

    Add factory functions for new to-do lists and new projects

---
+++ src/factories.js
@@ -0,0 +1,9 @@
+const todo = (title, description, dueDate, priority, notes) => {
+    return {title, description, dueDate, priority, notes};
+};
+
+const project = (projectName) => {
+    return projectName = [];
+};
+
+export {todo, project};
