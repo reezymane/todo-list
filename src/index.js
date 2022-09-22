bfc27a39da022ef066b6c8e3a9523a90baaf2d6a
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-22 16:28:18 -0500

    Import open/close form functions

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
 import './style.css';
 import {todo, project} from './factories';
+import {openPForm, closePForm, openTDForm, closeTDForm} from './functions';
 
 (() => {
-    // Display and hide forms
-    function openPForm() {
-        document.getElementById('newProject').style.display = 'block';
-    };
-
-    function closePForm() {
-        document.getElementById('newProject').style.display = 'none';
-        document.getElementById('newProject').reset();
-    };
-
-    function openTDForm() {
-        document.getElementById('todo').style.display = 'block';
-    };
-
-    function closeTDForm() {
-        document.getElementById('todo').style.display = 'none';
-        document.getElementById('todo').reset();
-    };
-
     // Makes new project form appear when new project button is clicked
     const newProject = document.getElementsByClassName('newProject');
     newProject.item(0).addEventListener('click', () => {
