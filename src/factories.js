0afdaf6195cfe851ee1d4dcb6699d171e7364c6a
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-22 15:44:46 -0500

    Return an object for projects instead of array

--- src/factories.js
+++ src/factories.js
@@ -1,9 +1,11 @@
+// Stores to-do details into an object
 const todo = (title, description, dueDate, priority, notes) => {
     return {title, description, dueDate, priority, notes};
 };
 
+// Stores project name and an array to hold to-do's into an object
 const project = (projectName) => {
-    return projectName = [];
+    return {name: projectName, list: []};
 };
 
 export {todo, project};
