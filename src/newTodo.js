612ff1094458ac1ec2d02b6474524778dd3de930
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-26 12:02:42 -0500

    Add function to display to-do list content

--- src/newTodo.js
+++ src/newTodo.js
@@ -11,6 +11,68 @@ const todoPri = () => {
     };
 };
 
+// Displays to-do
+const displayTodo = (list) => {
+    const todoList = document.getElementsByClassName('todoList');
+    const todoHeading = document.createElement('div');
+    todoHeading.setAttribute('id', list.title + 'Heading');
+    todoList.item(0).appendChild(todoHeading);
+
+    const innerHeading = document.createElement('div');
+    innerHeading.setAttribute('id', list.title + 'Inner');
+    todoHeading.appendChild(innerHeading);
+
+    // Adds mark complete button
+    const completeButton = document.createElement('button');
+    completeButton.setAttribute('id', list.title + 'Complete');
+    innerHeading.appendChild(completeButton);
+
+    // Adds to-do title
+    const todoTitle = document.createElement('p');
+    todoTitle.textContent = list.title;
+    innerHeading.appendChild(todoTitle);
+
+    // Adds to-do description
+    const todoDescription = document.createElement('p');
+    todoDescription.textContent = list.description;
+    innerHeading.appendChild(todoDescription);
+
+    // Creates div for dueDate display
+    const toDueDisplay = document.createElement('div');
+    toDueDisplay.setAttribute('id', list.title + 'toDueDisplay');
+    todoList.item(0).appendChild(toDueDisplay);
+
+    // Adds 'Due' text
+    const toDueText = document.createElement('p');
+    toDueText.textContent = 'Due:';
+    toDueDisplay.appendChild(toDueText);
+
+    // Adds to-do dueDate
+    const toDueDate = document.createElement('p');
+    toDueDate.textContent = list.dueDate;
+    toDueDisplay.appendChild(toDueDate);
+
+    // Creates div for priority, notes, and remove/change priority buttons
+    const hiddenDiv = document.createElement('div');
+    hiddenDiv.setAttribute('id', list.title + 'HiddenDiv');
+    todoList.item(0).appendChild(hiddenDiv);
+
+    // Creates inner div for priority and notes
+    const priorityNotes = document.createElement('div');
+    priorityNotes.setAttribute('id', list.title + 'PriorityNotes');
+    hiddenDiv.appendChild(priorityNotes);
+
+    // Adds priority
+    const toPriority = document.createElement('p');
+    toPriority.textContent = list.priority;
+    priorityNotes.appendChild(toPriority);
+
+    // Adds notes
+    const toNotes = document.createElement('p');
+    toNotes.textContent = list.notes;
+    priorityNotes.appendChild(toNotes);
+};
+
 // Submits a new to-do object to projects.list array
 const submitTodo = () => {
     const todoSubmit = document.getElementById('todoSubmit');
@@ -19,8 +81,11 @@ const submitTodo = () => {
             if (currentProject.name === object.name) {
                 object.list.push(todo(document.getElementById('todoTitle').value,
                 document.getElementById('todoDescription').value,
+                document.getElementById('todoDueDate').value,
                 todoPri(),
                 document.getElementById('notes').value));
+
+                displayTodo(object.list[(object.list.length - 1)]);
 
                 closeTDForm();
             };
