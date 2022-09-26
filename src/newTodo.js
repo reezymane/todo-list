b994f1e4f74e91739035f68b820b3af1b3a3dbb3
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-26 10:28:45 -0500

    Create and export function to submit to-do to current project

---
+++ src/newTodo.js
@@ -0,0 +1,33 @@
+import {todo, projects, currentProject} from './factories';
+import {closeTDForm} from './functions';
+
+// Gets value for to-do radio button selection
+const todoPri = () => {
+    const todoRadio = document.getElementsByName('todoPriority');
+    for (let i = 0; i < todoRadio.length; i++) {
+        if (todoRadio[i].checked) {
+            return todoRadio[i].value;
+        };
+    };
+};
+
+// Submits a new to-do object to projects.list array
+const submitTodo = () => {
+    const todoSubmit = document.getElementById('todoSubmit');
+    todoSubmit.addEventListener('click', () => {
+        projects.list.forEach((object) => {
+            if (currentProject.name === object.name) {
+                object.list.push(todo(document.getElementById('todoTitle').value,
+                document.getElementById('todoDescription').value,
+                todoPri(),
+                document.getElementById('notes').value));
+
+                closeTDForm();
+
+                console.log(object);
+            };
+        });
+    });
+};
+
+export {submitTodo};
