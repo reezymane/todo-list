cfa2b47e1654daf3d7de1fe12ef1e32f89d76612
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-26 12:13:38 -0500

    Add block that Removes existing to-do list in generalClick

--- src/index.js
+++ src/index.js
@@ -29,6 +29,13 @@ import {submitTodo} from "./newTodo";
             projectDue.item(0).removeChild(projectDue.item(0).firstChild);
         };
 
+        // Removes existing to-do list
+        const currentTodo = document.getElementsByClassName('todoList');
+
+        while (currentTodo.item(0).firstChild != null) {
+            currentTodo.item(0).removeChild(currentTodo.item(0).firstChild);
+        };
+
         generalProject();
     });
 
