93cbca995be2f3cce446a22b9d2412ef87e9fb98
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-30 14:57:57 -0500

    Add projectHome property to todo constructor

--- src/factories.js
+++ src/factories.js
@@ -1,6 +1,6 @@
 // Stores to-do details into an object
-const todo = (title, description, dueDate, priority, notes) => {
-    return {title, description, dueDate, priority, notes};
+const todo = (projectHome, title, description, dueDate, priority, notes) => {
+    return {projectHome, title, description, dueDate, priority, notes};
 };
 
 // Stores project name and an array to hold to-do's into an object
